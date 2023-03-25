0f47a1d061b744c6abb3768d417603c5fff71fab
qijackson5 <77818221+qijackson5@users.noreply.github.com>
2023-03-25 13:39:42 -0700

    working changes;

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -9,6 +9,15 @@ def index():
         salary = float(request.form['salary'])
         return render_template('index.html', salary=salary)
     return render_template('index.html')
+
+
+@app.route('/earnings')
+def get_earnings():
+    salary = float(request.args.get('salary'))
+    elapsed_time = float(request.args.get('elapsed_time'))
+    earnings_per_second = salary / 31536000  # 365 days * 24 hours * 60 minutes * 60 seconds
+    earnings = earnings_per_second * elapsed_time
+    return jsonify({'earnings': earnings})
 
 
 if __name__ == '__main__':
